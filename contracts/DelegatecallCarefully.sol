1f2eb9032d791f861c01e20a82bf5133946ae9ee
jeff mascarenhas <jeftarmascarenhas@hotmail.com>
2023-12-03 23:16:39 -0300

    feat: creating delegatecall

---
+++ contracts/DelegatecallCarefully.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+/*
+* Delegatecall é um chamada de mensagem que executa um código de um contrato dentro do outro
+* Porem contexto da execução é o do contrato chamador, ou seja se você chama uma função
+* Do Contrato A dentro do Contrato B a excursão e alterações de estado será feito no contrato B
+* e não no contrato A.
+*/
+
+/*
+* Delegatecall afeta as variáveis ​​de estado do contrato que chama uma função com delegadocall.
+* As variáveis ​​de estado do contrato que contém as funções emprestadas não são lidas nem gravadas.
+*/
+contract CotractCalled {
+  uint8 public num;
+  address public owner;
+  uint256 public time;
+  string public message;
+  bytes public data;
+
+
+  function callOne() public{
+      num = 100;
+      owner = msg.sender;
+      time = block.timestamp;
+      message = "Darah";
+      data = abi.encodePacked(num, msg.sender, block.timestamp);
+
+  }
+}
+
+
+contract CallerContract {
+
+  uint8 public num;
+  address public owner;
+  uint256 public time;
+  string public message;
+  bytes public data;
+
+  function callTwo(address contractAddress) public returns(bool){
+
+      (bool success,) = contractAddress.delegatecall(
+          abi.encodeWithSignature("callOne()")
+      );
+      return success;
+    }
+     
+}
