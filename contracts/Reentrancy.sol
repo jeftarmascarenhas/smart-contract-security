279ba5f15f9fa9ea0beacefd0f4f7897b715fdc6
jeff mascarenhas <jeftarmascarenhas@hotmail.com>
2023-12-03 23:22:51 -0300

    feat: adding extra text

--- contracts/Reentrancy.sol
+++ contracts/Reentrancy.sol
@@ -1,6 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
+/**
+ * A reentrada é um método de programação em que uma chamada de função externa faz com que a execução de uma função seja pausada. 
+ * As condições na lógica da chamada de função externa permitem que ela se chame repetidamente antes que a execução 
+ * original da função seja concluída
+ */
+
+
+/**
+ * ReentrancyGuard contrato que utiliza um modificador para evitar ataque de reentrada
+*/
 abstract contract ReentrancyGuard {
   error NoReentrancy();
   bool locked;
@@ -15,13 +25,20 @@ abstract contract ReentrancyGuard {
   }
 }
 
-contract Victim is ReentrancyGuard {
+/**
+ * Victim contrato sem validação para evitar reentrada, que será nosso contrato vítima.
+ * Para evitar o ataque herde do contrato ReentrancyGuard o modificador noReentrancy
+*/
+contract Victim {
     mapping(address => uint256) public balances;
 
     function deposit() external payable {
         balances[msg.sender] += msg.value;
     }
-    // Use o modifier noReentrancy, caso contrário a função abaixo pode sofre ataques.
+    /**
+     * Use o modifier noReentrancy, caso contrário a função abaixo pode sofre ataques.
+     * ex: function withdraw() external noReentrancy {}
+     */
     function withdraw() external  {
         uint256 balance = balances[msg.sender];
         require(balance > 0, "value is less than 0");
@@ -30,6 +47,9 @@ contract Victim is ReentrancyGuard {
     }
 }
 
+/**
+ * Attack contrato que fará o ataque de reentrada no contrato Victim
+*/
 contract Attack {
     Victim public victim;
 
